e8efb57228cd985437ebfdc56f15a8966e446390
sairamya68 <vallamsettisairamya@gmail.com>
2024-06-18 05:48:50 -0400

    components

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +15,8 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Hello World</h1>
+        <p>App Developer</p>
       </header>
     </div>
   );
